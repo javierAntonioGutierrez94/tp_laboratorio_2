7bd0a781b2b3d43d2556be6ad6c8d5f4ec155194
javierAntonioGutierrez94 <52746811+javierAntonioGutierrez94@users.noreply.github.com>
2020-10-05 01:07:00 -0300

    entrega 5/10/20

---
+++ Javier_Gutierrez_2D_TP-02/Entidades/Suv.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv : Vehiculo
+    {
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return Vehiculo.ETamanio.Grande;
+            }
+        }
+
+
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+            : base(marca, chasis, color)
+        {
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
